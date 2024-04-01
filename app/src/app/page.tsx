ed69ad1ee630d961a8478230cfccf2b7ebd1e179
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-04-01 17:36:41 -0600

    NextJS installed

---
+++ app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1>BeatHub</h1>
+        <p>Ahora si bro a trabajar se ha dichooo!</p>
+      </div>
+    </main>
+  );
+}
