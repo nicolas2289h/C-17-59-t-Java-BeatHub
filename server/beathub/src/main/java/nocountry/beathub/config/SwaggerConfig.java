0e2eca1368073204e62a799f14ccc6b1421a7714
Lstortoni <89618746+Lstortoni@users.noreply.github.com>
2024-04-03 11:15:57 -0300

    Swagger setting
    Agregue algunas configuraciones para Swagger

--- server/beathub/src/main/java/nocountry/beathub/config/SwaggerConfig.java
+++ server/beathub/src/main/java/nocountry/beathub/config/SwaggerConfig.java
@@ -1,8 +1,30 @@
 package nocountry.beathub.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 
 public class SwaggerConfig {
+
+    /**
+     * Configurar el bean de Open API para cargar la informacion basica del proyecto
+     * @return open api info
+     */
+    @Bean
+    public OpenAPI themeparkOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("BeatHub spring boot 3")
+                        .description("Aplicación de API Rest para el crud de BeatHub")
+                        .version("v1.0.0")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+    }
+
 }
