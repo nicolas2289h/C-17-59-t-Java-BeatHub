081653e3de025f1dbf467ee033b217775a4d65f0
Agustin Piuca <aguspiucaquevedo@gmail.com>
2024-04-07 18:34:46 -0300

    Diseño signUp hecho

---
+++ app/src/components/app/registrar-opc/ClientSignUpOption.tsx
@@ -0,0 +1,12 @@
+import RegisterComponent from "./registerComponent/RegisterComponent";
+
+const ClientSignUpOption = () => {
+    return (
+        <section className="w-[80%] mx-auto bg-black text-white">
+            <RegisterComponent/>
+            <RegisterComponent/>
+        </section>
+    )
+}
+
+export default ClientSignUpOption;
---
+++ app/src/components/app/registrar-opc/registerComponent/RegisterComponent.tsx
@@ -0,0 +1,20 @@
+const RegisterComponent = () => {
+    return (
+        <div className='bg-zinc-300 grid grid-cols-5 gap-4'>
+            <figure className="w-[150px] h-[75px] bg-gray-500 col-span-2">
+
+            </figure>
+            <div className="col-span-3">
+                <h3>Productor</h3>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aut velit id fuga eligendi ut voluptatem ipsa reiciendis a adipisci, nisi ducimus veniam consequatur in? Quas facilis quae beatae cupiditate?</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates beatae blanditiis sunt eum sed similique tempore facilis qui, neque magnam. Magni placeat fugiat a quae minima quas sint temporibus dolorum?</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nostrum ipsa? Reiciendis ab officia, porro a exercitationem culpa perspiciatis eius quasi consequuntur fugiat. Ullam eligendi reiciendis ut a fugiat repudiandae.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa dicta amet dolor ab numquam, temporibus error, id consequuntur rem voluptatibus, molestiae at vitae adipisci sed minus impedit mollitia magnam. Quibusdam?</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default RegisterComponent;
