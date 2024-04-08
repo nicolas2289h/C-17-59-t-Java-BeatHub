a31baaebc15180083ad4d8d65d52afbd42106cb4
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-04-08 11:31:16 -0600

    Refactor SignUpOpc component

--- app/src/app/registrar-opc/page.tsx
+++ app/src/app/registrar-opc/page.tsx
@@ -1,11 +1,11 @@
 import ClientSignUpOption from "@/components/app/registrar-opc/ClientSignUpOption";
 
 const SignUpOpc = () => {
-    return (
-        <>
-            <ClientSignUpOption/>
-        </>
-    )
-}
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ClientSignUpOption />
+    </main>
+  );
+};
 
 export default SignUpOpc;
