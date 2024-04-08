e4ad95d9409bf602b0dcb2cbed2308f248fa84af
Nico <nicolas2289h@gmail.com>
2024-04-08 19:27:17 -0300

    Cambio de nombre Cantante a Artista

--- server/beathub/src/main/java/nocountry/beathub/service/IArtistaService.java
+++ server/beathub/src/main/java/nocountry/beathub/service/IArtistaService.java
@@ -1,18 +1,18 @@
 package nocountry.beathub.service;
 
-import nocountry.beathub.exception.CantanteExistException;
+import nocountry.beathub.exception.ArtistaExistException;
 import nocountry.beathub.exception.HibernateOperationException;
 import nocountry.beathub.exception.IncorrectPasswordException;
 import nocountry.beathub.exception.UsernameNotFoundException;
-import nocountry.beathub.model.Cantante;
+import nocountry.beathub.model.Artista;
 
 import java.util.List;
 
-public interface ICantanteService {
-    boolean registerCantante(Cantante cantante)throws CantanteExistException,HibernateOperationException;
+public interface IArtistaService {
+    boolean registerArtista(Artista artista)throws ArtistaExistException,HibernateOperationException;
 
     boolean loginUser(String username, String password)throws UsernameNotFoundException, IncorrectPasswordException, HibernateOperationException;
 
-    List<Cantante> findAllCantantes();
+    List<Artista> findAllArtistas();
 }
 
