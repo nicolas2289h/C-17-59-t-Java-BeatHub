741cc9f980e51fa017d47b47a9688905cf17e845
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-04-08 21:50:24 -0600

    Refactor header component and music player button, added functionally for shopping cart

--- app/src/components/layout/musicPlayer/MusicPlayerButtonAddToCart.tsx
+++ app/src/components/layout/musicPlayer/MusicPlayerButtonAddToCart.tsx
@@ -1,8 +1,13 @@
 import { formatNumber } from "@/components/utils/dataFormat";
+import {
+  getLocalStorage,
+  setLocalStorage,
+} from "@/components/utils/handleLocalStorage";
 import { $ShoppingCart, PropsBeat } from "@/stores/beats";
 import { useStore } from "@nanostores/react";
-import { Tooltip } from "@nextui-org/react";
+import { Button, Tooltip } from "@nextui-org/react";
 import { IconShoppingCartPlus } from "@tabler/icons-react";
+import { useEffect } from "react";
 import toast from "react-hot-toast";
 
 export const MusicPlayerButtonAddToCart = ({ beat }: { beat: PropsBeat }) => {
@@ -11,22 +16,35 @@ export const MusicPlayerButtonAddToCart = ({ beat }: { beat: PropsBeat }) => {
     if (shoppingCart && shoppingCart.find((b) => b.id === beat.id)) return;
     if (!shoppingCart) {
       $ShoppingCart.set([beat]);
-      toast.success(`"${beat.name}" agregado al carrito`, {
-        position: "top-center",
-        duration: 3000,
-      });
     } else {
       $ShoppingCart.set([...shoppingCart, beat]);
-      toast.success(`"${beat.name}" agregado al carrito`, {
-        position: "top-center",
-        duration: 3000,
-      });
     }
+    toast.success(`"${beat.name}" agregado al carrito`, {
+      position: "top-center",
+      duration: 3000,
+    });
   };
+  useEffect(() => {
+    if (shoppingCart) {
+      return setLocalStorage(`localShoppingCart`, shoppingCart);
+    } else if (
+      shoppingCart === null &&
+      localStorage.getItem(`localShoppingCart`)
+    ) {
+      return $ShoppingCart.set(getLocalStorage(`localShoppingCart`));
+    }
+  }, [shoppingCart]);
   return (
     <Tooltip content="Licencia exclusiva">
-      <button
-        className="w-[7rem] h-[2.7rem] p-1 flex items-center justify-center gap-1 rounded-lg shadow-md ring-1 bg-red-500 text-terciario ring-secundario/5 active:scale-95 duration-100 ease-out hover:shadow-lg hover:scale-105 text-lg"
+      <Button
+        disabled={
+          shoppingCart === null
+            ? false
+            : shoppingCart.find((b) => b.id === beat.id)
+            ? true
+            : false
+        }
+        className="disabled:opacity-65 disabled:hover:opacity-65 disabled:scale-100 disabled:shadow-md disabled:active:scale-100 w-[7rem] h-[2.7rem] p-1 flex items-center justify-center gap-1 rounded-lg shadow-md ring-1 bg-red-500 text-terciario ring-secundario/5 active:scale-95 duration-100 ease-out hover:shadow-lg hover:scale-105 text-lg"
         onClick={handleAddBeatToCart}
       >
         <IconShoppingCartPlus
@@ -40,7 +58,7 @@ export const MusicPlayerButtonAddToCart = ({ beat }: { beat: PropsBeat }) => {
             $ {formatNumber(beat.price)}
           </small>
         </div>
-      </button>
+      </Button>
     </Tooltip>
   );
 };
