04aafb67fce5e3432337993e6d9ed54d990b72bb
Agustin Piuca <aguspiucaquevedo@gmail.com>
2024-04-23 03:23:51 -0300

    font roboto added

--- app/src/components/sections/beatsPlaylists/PlaylistBeatsSection.tsx
+++ app/src/components/sections/beatsPlaylists/PlaylistBeatsSection.tsx
@@ -35,24 +35,27 @@ export const PlaylistBeatsSection = ({
             "shadow-xl ring-1 ring-secundario/10 scale-105"
           } bg-blanco w-[20rem] relative cursor-pointer flex flex-col gap-1 p-4 rounded-xl transition-all duration-300 ease-in-out overflow-hidden`}
         >
-          <img
-            src={`https://img.youtube.com/vi/${beat.url}/mqdefault.jpg`}
-            alt={`imagen de ${beat.nombre}`}
-            className="rounded-xl w-full h-[10rem] object-cover"
-          />
           <div className="flex items-center justify-center">
             <div className="w-1/2">
-              <h3 className="text-secundario/80 font-semibold text-lg">
-                {beat.nombre}
-              </h3>
-              <h4 className="text-sm text-secundario/50">
+              <h3 className="text-sm text-secundario/50">
                 {beat.productor.username}
+              </h3>
+              <small className="">
+                {beat.genero}
+              </small>
+              <h4 className="text-secundario/80 font-semibold text-lg">
+                {beat.nombre}
               </h4>
             </div>
             <div className={`w-1/2 flex items-center justify-end`}>
               <MusicPlayerButtonAddToCart beat={beat} />
             </div>
           </div>
+          <img
+            src={`https://img.youtube.com/vi/${beat.url}/mqdefault.jpg`}
+            alt={`imagen de ${beat.nombre}`}
+            className="rounded-[14px] w-full h-[10rem] object-cover"
+          />
           <div className="flex absolute bg-secundario/10 text-slate-200 gap-2 justify-center p-1 rounded-xl right-4">
             <small>{beat.tiempoBpm}bpm,</small>
             <small>{beat.tonalidad}</small>
